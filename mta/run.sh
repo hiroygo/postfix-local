01d62b5c1ed77aeeed04f92885c286259e0048e5
hiroygo <pcxn1a@gmail.com>
2022-12-01 11:06:59 +0900

    `postfix stop` before the container stops

--- mta/run.sh
+++ mta/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+trap 'postfix stop' SIGTERM
+
 # `/etc/resolv.conf` is rewritten at container startup.
 # So, Postfix cannot resolve the name when copied in the Dockerfile.
 cp /etc/resolv.conf /var/spool/postfix/etc/
 
 postmap /etc/postfix/transport
-postfix start-fg
+
+postfix start-fg &
+wait $!
+echo bye
