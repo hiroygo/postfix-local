9ef1b2faa6c0874e6eb7a3104835d6213d31d970
hiroygo <pcxn1a@gmail.com>
2022-12-02 01:11:38 +0900

    wait for the master daemon's child process to exit

--- mta/run.sh
+++ mta/run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-trap 'postfix stop' SIGTERM
+shutdown_handler() {
+    pids=$(pgrep -P $(cat /var/spool/postfix/pid/master.pid))
+    postfix stop
+
+    for i in $pids
+    do
+        # https://unix.stackexchange.com/questions/103862/how-do-i-wait-on-a-program-started-in-another-shell
+        while $(ps -p $i > /dev/null 2>&1)
+        do
+            sleep 0.1
+        done
+    done
+}
+trap shutdown_handler SIGTERM
 
 # `/etc/resolv.conf` is rewritten at container startup.
 # So, Postfix cannot resolve the name when copied in the Dockerfile.
